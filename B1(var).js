0c29776293681eebcec5ccc5a094d43ff4429857
smartkurs <62181488+smartkurs@users.noreply.github.com>
2020-03-18 15:38:19 +0600

    smart B1

---
+++ B1(var).js
@@ -0,0 +1,1 @@
+var OEConfWEDisableRightClick = {"WE8b4bb6d7c7":{"Text":{"Items":{"DEFAULT":"Clic droit interdit"}}}}
---
+++ index(var).js
@@ -0,0 +1,1 @@
+var OEConfWEDisableRightClick = {"WE733207143b":{"Text":{"Items":{"DEFAULT":"Clic droit interdit"}}}}
